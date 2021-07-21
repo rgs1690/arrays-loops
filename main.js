1c2b80921b298179909085cf62dd6d096db9c3f0
Grace Sutherland <rgs1690@gmail.com>
2021-07-20 21:27:04 -0500

    added objects in array

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 console.log("Arrays and Loops!")
+const team5 = [
+let  user1 = { 
+    id: 1,
+    name: "Grace",
+    email: "rgs1690@gmail.com",
+    faveColor: "green",
+    uniqueTrait: "Is a twin",
+    img: "https://avatars.githubusercontent.com/u/78558344?v=4"
+},
+
+let user2 = { 
+    id: 2,
+    name: "Derek M",
+    email: "laxmalone@gmail.com",
+    faveColor: "blue",
+    uniqueTrait: "arthritic knees", 
+    img: "https://avatars.githubusercontent.com/u/77118878?v=4"
+},
+
+let user3 = { 
+    id: 3,
+    name: "Meseret",
+    email: "negash4@gmail.com",
+    faveColor: "tumeric yellow" ,
+    uniqueTrait: "has 9 siblings",
+    img: "https://avatars.githubusercontent.com/u/86808221?v=4"
+},
+
+let user4 = { 
+    id: 4,
+    name: "Derek B",
+    email: "dbaylor25@gmail.com",
+    faveColor: "green",
+    uniqueTrait: "excellent vision",
+    img: "https://avatars.githubusercontent.com/u/83030603?v=4"
+}
+];
